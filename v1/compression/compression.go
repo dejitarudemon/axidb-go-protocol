f517f3961e768d0def309303c2a528633a76c90f
Vlad Pep <fine.usr1@yandex.ru>
2026-09-19 19:57:32 +0300

    code: added Compression struct

--- v1/compression/compression.go
+++ v1/compression/compression.go
@@ -1,5 +1,11 @@
 package compression
 
+import (
+	"fmt"
+
+	"github.com/dejitarudemon/axidb-go-protocol/v1/buffer"
+)
+
 type Compression uint8
 
 const (
@@ -9,3 +15,20 @@ const (
 )
 
 const CompressionFieldSize = 1
+
+func (c Compression) Encode(buf buffer.Appender) {
+	buf.AppendUint8(uint8(c))
+}
+
+func (c Compression) String() string {
+	switch c {
+	case None:
+		return "None"
+	case Zstd:
+		return "Zstd"
+	case Lz4:
+		return "Lz4"
+	}
+
+	return fmt.Sprintf("Unknown (%d)", c)
+}
