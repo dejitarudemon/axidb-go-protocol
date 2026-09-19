2d177641b86035a73dd03facea3f0db77940deca
Vlad Pep <fine.usr1@yandex.ru>
2026-09-19 22:05:09 +0300

    refactoring: added godocs

--- v1/command/command.go
+++ v1/command/command.go
@@ -1,3 +1,12 @@
+/*
+package command предназначен для представления кодов
+команд (Command) согласно спецификации протокола v1.
+
+Использование:
+
+	с = Code(1)
+*/
+
 package command
 
 import (
@@ -7,10 +16,22 @@ import (
 	"github.com/dejitarudemon/axidb-go-protocol/v1/error/errs"
 )
 
+/*
+type Code предназначен для хранения
+кода команды, его валидации и кодирования в сообщение.
+*/
 type Code uint8
 
+/*
+FieldSize представляет размер в байтах,
+отведенный для хранения кода команды в сообщении.
+*/
 const FieldSize = 1
 
+/*
+Константы, представляющие команды,
+используемые в спецификации протокола v1.
+*/
 const (
 	Handshake Code = iota
 	Answer
@@ -21,10 +42,21 @@ const (
 	Ping
 )
 
+/*
+func Encode предназначена для кодирования кода команды
+в сообщении.
+
+Принимааемые параметры:
+  - buf buffer.Appender - буфер для хранения закодированного значения.
+*/
 func (c Code) Encode(buf buffer.Appender) {
 	buf.AppendUint8(uint8(c))
 }
 
+/*
+func String предназначена для вывода человекочитаемого названия
+команды, представленного конкретным кодом.
+*/
 func (c Code) String() string {
 	switch c {
 	case Handshake:
@@ -46,6 +78,14 @@ func (c Code) String() string {
 	return fmt.Sprintf("Unknown (%d)", c)
 }
 
+/*
+func IsValid предназначена для проверки кода команды.
+Проверки:
+ 1. Код находится в пределах 0-6.
+
+Возвращаемые ошибки:
+ 1. ErrorUnsupportedCommand - код находится вне пределов 0-6.
+*/
 func (c Code) IsValid() error {
 	if c > Ping {
 		return errs.NewErrorUnsupportedCommand(uint8(c))
@@ -54,6 +94,9 @@ func (c Code) IsValid() error {
 	return nil
 }
 
+/*
+func Size возвращает размер кода команды в байтах.
+*/
 func (c Code) Size() int {
 	return FieldSize
 }
