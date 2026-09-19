2a79c841056514ecab8c1dd0470460ecee462840
Vlad Pep <fine.usr1@yandex.ru>
2026-09-19 21:35:06 +0300

    test: added all tests for Compression

--- v1/compression/compression_test.go
+++ v1/compression/compression_test.go
@@ -8,6 +8,30 @@ import (
 	"github.com/dejitarudemon/axidb-go-protocol/v1/buffer"
 )
 
+func TestCompression_Size(t *testing.T) {
+	tests := []struct {
+		c    Compression
+		want int
+	}{
+		{None, 1},
+		{Zstd, 1},
+		{Lz4, 1},
+		{Compression(3), 1},
+		{Compression(255), 1},
+	}
+
+	for _, tt := range tests {
+		t.Run(
+			fmt.Sprintf("%v", tt.c),
+			func(t *testing.T) {
+				if got := tt.c.Size(); got != tt.want {
+					t.Errorf("got %v, want %v", got, tt.want)
+				}
+			},
+		)
+	}
+}
+
 func TestCompression_Encode(t *testing.T) {
 	tests := []struct {
 		c    Compression
@@ -34,6 +58,54 @@ func TestCompression_Encode(t *testing.T) {
 
 				if !bytes.Equal(got, tt.want) {
 					t.Errorf("Encode() = %q, want %q", got, tt.want)
+				}
+			},
+		)
+	}
+}
+
+func TestCompression_String(t *testing.T) {
+	tests := []struct {
+		c    Compression
+		want string
+	}{
+		{None, "None"},
+		{Zstd, "Zstd"},
+		{Lz4, "Lz4"},
+		{Compression(3), "Unknown (3)"},
+		{Compression(255), "Unknown (255)"},
+	}
+
+	for _, tt := range tests {
+		t.Run(
+			fmt.Sprintf("%v", tt.c),
+			func(t *testing.T) {
+				if got := tt.c.String(); got != tt.want {
+					t.Errorf("got %v, want %v", got, tt.want)
+				}
+			},
+		)
+	}
+}
+
+func TestCompression_IsValid(t *testing.T) {
+	tests := []struct {
+		c       Compression
+		wantErr bool
+	}{
+		{None, false},
+		{Zstd, false},
+		{Lz4, false},
+		{Compression(3), true},
+		{Compression(255), true},
+	}
+
+	for _, tt := range tests {
+		t.Run(
+			fmt.Sprintf("%v", tt.c),
+			func(t *testing.T) {
+				if got := tt.c.IsValid(); got == nil == tt.wantErr {
+					t.Errorf("got %v, want %v", got, tt.wantErr)
 				}
 			},
 		)
