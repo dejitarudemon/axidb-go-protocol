c1bb0249c838d15fc4777a659adcb1da7de81042
Vlad Pep <fine.usr1@yandex.ru>
2026-09-19 21:17:24 +0300

    code: fix methods doesn't save data

--- v1/buffer/mock.go
+++ v1/buffer/mock.go
@@ -6,40 +6,42 @@ var (
 	_ Buffer = (*BufferMock)(nil)
 )
 
-type BufferMock []byte
-
-func (b BufferMock) Preallocate(size int) {
-	b = make(BufferMock, 0, size)
+type BufferMock struct {
+	data []byte
 }
 
-func (b BufferMock) Clean() {
-	b = b[:0]
+func (b *BufferMock) Preallocate(size int) {
+	b.data = make([]byte, 0, size)
 }
 
-func (b BufferMock) Bytes() []byte {
-	return b
+func (b *BufferMock) Clean() {
+	b.data = b.data[:0]
 }
 
-func (a BufferMock) Append(v []byte) {
-	a = append(a, v...)
+func (b *BufferMock) Bytes() []byte {
+	return b.data
 }
 
-func (a BufferMock) AppendString(v string) {
-	a = append(a, v...)
+func (b *BufferMock) Append(v []byte) {
+	b.data = append(b.data, v...)
 }
 
-func (a BufferMock) AppendUint8(v uint8) {
-	a = append(a, byte(v))
+func (b *BufferMock) AppendString(v string) {
+	b.data = append(b.data, v...)
 }
 
-func (a BufferMock) AppendUint16(v uint16) {
-	a = binary.BigEndian.AppendUint16(a, v)
+func (b *BufferMock) AppendUint8(v uint8) {
+	b.data = append(b.data, byte(v))
 }
 
-func (a BufferMock) AppendUint32(v uint32) {
-	a = binary.BigEndian.AppendUint32(a, v)
+func (b *BufferMock) AppendUint16(v uint16) {
+	b.data = binary.BigEndian.AppendUint16(b.data, v)
 }
 
-func (a BufferMock) AppendUint64(v uint64) {
-	a = binary.BigEndian.AppendUint64(a, v)
+func (b *BufferMock) AppendUint32(v uint32) {
+	b.data = binary.BigEndian.AppendUint32(b.data, v)
+}
+
+func (b *BufferMock) AppendUint64(v uint64) {
+	b.data = binary.BigEndian.AppendUint64(b.data, v)
 }
