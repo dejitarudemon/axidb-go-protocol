e08be21da67866bfe23c94d5f47aebb292500f87
Vlad Pep <fine.usr1@yandex.ru>
2026-09-19 21:17:44 +0300

    test: added test for Compression.Encode()

---
+++ v1/compression/compression_test.go
@@ -0,0 +1,41 @@
+package compression
+
+import (
+	"bytes"
+	"fmt"
+	"testing"
+
+	"github.com/dejitarudemon/axidb-go-protocol/v1/buffer"
+)
+
+func TestCompression_Encode(t *testing.T) {
+	tests := []struct {
+		c    Compression
+		want []byte
+	}{
+		{None, []byte{0x00}},
+		{Zstd, []byte{0x01}},
+		{Lz4, []byte{0x02}},
+	}
+
+	for _, tt := range tests {
+		t.Run(
+			fmt.Sprintf("%v", tt.c),
+			func(t *testing.T) {
+				buf := buffer.BufferMock{}
+				buf.Preallocate(tt.c.Size())
+
+				tt.c.Encode(&buf)
+				got := buf.Bytes()
+
+				if len(got) != tt.c.Size() {
+					t.Fatalf("expected %v bytes, got %v bytes", tt.c.Size(), len(got))
+				}
+
+				if !bytes.Equal(got, tt.want) {
+					t.Errorf("Encode() = %q, want %q", got, tt.want)
+				}
+			},
+		)
+	}
+}
