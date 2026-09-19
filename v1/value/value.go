dedcdbb6b79d8f3c1464b9d790ffbf52342c18f1
Vlad Pep <fine.usr1@yandex.ru>
2026-09-19 22:28:37 +0300

    code: added v interface

---
+++ v1/value/value.go
@@ -0,0 +1,37 @@
+package value
+
+import "github.com/dejitarudemon/axidb-go-protocol/v1/buffer"
+
+/*
+interface V предназначен для объявления требований
+к данным (Value).
+*/
+type V interface {
+	/*
+		func Size возвращает размер данных в байтах.
+	*/
+	Size() int
+
+	/*
+		func IsValid предназначена для проверки данных.
+		Проверки: зависят от конкретной реализации интерфейса.
+
+		Возвращаемые ошибки:
+		 1. ErrorMalformedValue.
+	*/
+	IsValid() error
+
+	/*
+		func Encode предназначена для кодирования данных
+		в сообщении.
+
+		Принимааемые параметры:
+		  - buf buffer.Appender - буфер для хранения закодированного значения.
+	*/
+	Encode(buf buffer.Appender)
+
+	/*
+		func Type возвращает код типа данных.
+	*/
+	Type() Code
+}
