8e1d1c3acf6caaad9cd212486e158caa98da35ee
Vlad Pep <fine.usr1@yandex.ru>
2026-09-19 21:22:12 +0300

    refactoring: renamed BufferMock -> Mock

--- v1/buffer/mock.go
+++ v1/buffer/mock.go
@@ -3,45 +3,45 @@ package buffer
 import "encoding/binary"
 
 var (
-	_ Buffer = (*BufferMock)(nil)
+	_ Buffer = (*Mock)(nil)
 )
 
-type BufferMock struct {
+type Mock struct {
 	data []byte
 }
 
-func (b *BufferMock) Preallocate(size int) {
-	b.data = make([]byte, 0, size)
+func (m *Mock) Preallocate(size int) {
+	m.data = make([]byte, 0, size)
 }
 
-func (b *BufferMock) Clean() {
-	b.data = b.data[:0]
+func (m *Mock) Clean() {
+	m.data = m.data[:0]
 }
 
-func (b *BufferMock) Bytes() []byte {
-	return b.data
+func (m *Mock) Bytes() []byte {
+	return m.data
 }
 
-func (b *BufferMock) Append(v []byte) {
-	b.data = append(b.data, v...)
+func (m *Mock) Append(v []byte) {
+	m.data = append(m.data, v...)
 }
 
-func (b *BufferMock) AppendString(v string) {
-	b.data = append(b.data, v...)
+func (m *Mock) AppendString(v string) {
+	m.data = append(m.data, v...)
 }
 
-func (b *BufferMock) AppendUint8(v uint8) {
-	b.data = append(b.data, byte(v))
+func (m *Mock) AppendUint8(v uint8) {
+	m.data = append(m.data, byte(v))
 }
 
-func (b *BufferMock) AppendUint16(v uint16) {
-	b.data = binary.BigEndian.AppendUint16(b.data, v)
+func (m *Mock) AppendUint16(v uint16) {
+	m.data = binary.BigEndian.AppendUint16(m.data, v)
 }
 
-func (b *BufferMock) AppendUint32(v uint32) {
-	b.data = binary.BigEndian.AppendUint32(b.data, v)
+func (m *Mock) AppendUint32(v uint32) {
+	m.data = binary.BigEndian.AppendUint32(m.data, v)
 }
 
-func (b *BufferMock) AppendUint64(v uint64) {
-	b.data = binary.BigEndian.AppendUint64(b.data, v)
+func (m *Mock) AppendUint64(v uint64) {
+	m.data = binary.BigEndian.AppendUint64(m.data, v)
 }
--- v1/compression/compression_test.go
+++ v1/compression/compression_test.go
@@ -22,7 +22,7 @@ func TestCompression_Encode(t *testing.T) {
 		t.Run(
 			fmt.Sprintf("%v", tt.c),
 			func(t *testing.T) {
-				buf := buffer.BufferMock{}
+				buf := buffer.Mock{}
 				buf.Preallocate(tt.c.Size())
 
 				tt.c.Encode(&buf)
