abe5765654c1b96ce9ac0fedf28ccb702237ace6
Vlad Pep <fine.usr1@yandex.ru>
2026-09-21 01:14:57 +0300

    code: added bodies.DeleteAnswer

---
+++ v1/body/bodies/delete_answer_test.go
@@ -0,0 +1,97 @@
+package bodies
+
+import (
+	"bytes"
+	"fmt"
+	"testing"
+
+	"github.com/dejitarudemon/axidb-go-protocol/v1/buffer"
+	"github.com/dejitarudemon/axidb-go-protocol/v1/command"
+)
+
+func TestDeleteAnswer_Size(t *testing.T) {
+	tests := []struct {
+		d    DeleteAnswer
+		want int
+	}{
+		{DeleteAnswer{}, 1},
+	}
+
+	for _, tt := range tests {
+		t.Run(
+			fmt.Sprintf("TestDeleteAnswer_Size %v", tt.d),
+			func(t *testing.T) {
+				if got := tt.d.Size(); got != tt.want {
+					t.Fatalf("got %v, want %v", got, tt.want)
+				}
+			},
+		)
+	}
+}
+
+func TestDeleteAnswer_Encode(t *testing.T) {
+	tests := []struct {
+		d    DeleteAnswer
+		want []byte
+	}{
+		{DeleteAnswer{}, []byte{0x01}},
+	}
+
+	for _, tt := range tests {
+		t.Run(
+			fmt.Sprintf("TestDeleteAnswer_Encode %v", tt.d),
+			func(t *testing.T) {
+				buf := buffer.Mock{}
+				buf.Preallocate(tt.d.Size())
+
+				tt.d.Encode(&buf)
+
+				got := buf.Bytes()
+
+				if !bytes.Equal(got, tt.want) {
+					t.Fatalf("got %q, want %q", got, tt.want)
+				}
+			},
+		)
+	}
+}
+
+func TestDeleteAnswer_Command(t *testing.T) {
+	tests := []struct {
+		d    DeleteAnswer
+		want command.Code
+	}{
+		{DeleteAnswer{}, command.Answer},
+	}
+
+	for _, tt := range tests {
+		t.Run(
+			fmt.Sprintf("TestDeleteAnswer_Command %v", tt.d),
+			func(t *testing.T) {
+				if got := tt.d.Command(); got != tt.want {
+					t.Fatalf("got %v, want %v", got, tt.want)
+				}
+			},
+		)
+	}
+}
+
+func TestDeleteAnswer_IsValid(t *testing.T) {
+	tests := []struct {
+		d    DeleteAnswer
+		want bool
+	}{
+		{DeleteAnswer{}, false},
+	}
+
+	for _, tt := range tests {
+		t.Run(
+			fmt.Sprintf("TestDeleteAnswer_IsValid %v", tt.d),
+			func(t *testing.T) {
+				if got := tt.d.IsValid(); got == nil == tt.want {
+					t.Fatalf("got %v, want %v", got, tt.want)
+				}
+			},
+		)
+	}
+}
