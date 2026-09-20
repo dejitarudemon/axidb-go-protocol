c645f17052321c104f059468c886c09ad4f09e0b
Vlad Pep <fine.usr1@yandex.ru>
2026-09-20 17:12:44 +0300

    code: added ErrorRequestInterrupted

---
+++ v1/err/errs/request_interrupted.go
@@ -0,0 +1,45 @@
+package errs
+
+import (
+	"fmt"
+
+	"github.com/google/uuid"
+
+	"github.com/dejitarudemon/axidb-go-protocol/v1/buffer"
+	"github.com/dejitarudemon/axidb-go-protocol/v1/err"
+)
+
+var _ err.Error = ErrorRequestInterrupted{}
+
+type ErrorRequestInterrupted struct {
+	interrupted uint32
+	tracebackID uuid.UUID
+}
+
+func NewErrorRequestInterrupted(interrupted uint32) ErrorRequestInterrupted {
+	return ErrorRequestInterrupted{
+		interrupted: interrupted,
+		tracebackID: generateNewTracebackID(),
+	}
+}
+
+func (e ErrorRequestInterrupted) TracebackID() uuid.UUID {
+	return e.tracebackID
+}
+
+func (e ErrorRequestInterrupted) Size() int {
+	return err.FieldSize + err.TracebackIDFieldSize
+}
+
+func (e ErrorRequestInterrupted) Code() err.Code {
+	return err.RequestInterrupted
+}
+
+func (e ErrorRequestInterrupted) Encode(buf buffer.Appender) {
+	buf.AppendUint16(uint16(e.Code()))
+	buf.Append(e.tracebackID[:])
+}
+
+func (e ErrorRequestInterrupted) Error() string {
+	return fmt.Sprintf("%v %v: id %v,", e.tracebackID, e.Code(), e.interrupted)
+}
