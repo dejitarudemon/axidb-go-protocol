a6179c57506524fe6677efdd32109145f434c2b8
Vlad Pep <fine.usr1@yandex.ru>
2026-09-20 13:29:01 +0300

    docs: added godoc

--- v1/error/protocol.go
+++ v1/error/protocol.go
@@ -5,15 +5,42 @@ import (
 	"github.com/google/uuid"
 )
 
+/*
+TracebackIDFieldSize представляет размер в байтах,
+отведенный для хранения Traceback ID ошибки в сообщении.
+*/
 const (
 	TracebackIDFieldSize = 16
 )
 
+/*
+interface Error предназначен для представления
+ошибки протокола согласно спецификации протокола v1.
+*/
 type Error interface {
 	error
 
+	/*
+		func Size возвращает размер сообщения ошибки в байтах.
+	*/
 	Size() int
+
+	/*
+		func TracebackID возвращает Traceback ID ошибки.
+	*/
 	TracebackID() uuid.UUID
+
+	/*
+		func Encode предназначена для кодирования сообщения об ошибке
+		в сообщении.
+
+		Принимаемые параметры:
+		  - buf buffer.Appender - буфер для хранения закодированного значения.
+	*/
 	Encode(buf buffer.Appender)
+
+	/*
+		func Code возвращает код ошибки.
+	*/
 	Code() Code
 }
