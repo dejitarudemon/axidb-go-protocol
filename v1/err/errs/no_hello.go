cd8ce0e60d44c6a59a0ca9fe472614ec55036392
Vlad Pep <fine.usr1@yandex.ru>
2026-09-20 15:39:16 +0300

    code: added ErrorNoHello

---
+++ v1/err/errs/no_hello.go
@@ -0,0 +1,42 @@
+package errs
+
+import (
+	"fmt"
+
+	"github.com/dejitarudemon/axidb-go-protocol/v1/buffer"
+	"github.com/dejitarudemon/axidb-go-protocol/v1/err"
+	"github.com/google/uuid"
+)
+
+var _ err.Error = ErrorNoHello{}
+
+type ErrorNoHello struct {
+	tracebackID uuid.UUID
+}
+
+func NewErrorNoHello(msg string) ErrorNoHello {
+	return ErrorNoHello{
+		tracebackID: generateNewTracebackID(),
+	}
+}
+
+func (e ErrorNoHello) TracebackID() uuid.UUID {
+	return e.tracebackID
+}
+
+func (e ErrorNoHello) Size() int {
+	return err.FieldSize + err.TracebackIDFieldSize
+}
+
+func (e ErrorNoHello) Code() err.Code {
+	return err.NoHello
+}
+
+func (e ErrorNoHello) Encode(buf buffer.Appender) {
+	buf.AppendUint16(uint16(e.Code()))
+	buf.Append(e.tracebackID[:])
+}
+
+func (e ErrorNoHello) Error() string {
+	return fmt.Sprintf("%v %v", e.tracebackID, e.Code())
+}
