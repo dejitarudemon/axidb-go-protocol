416b956d0f943bf54053d777a4dc37b564d16632
Vlad Pep <fine.usr1@yandex.ru>
2026-09-20 21:24:10 +0300

    code: added bodies.Write

---
+++ v1/body/bodies/write.go
@@ -0,0 +1,55 @@
+package bodies
+
+import (
+	"github.com/dejitarudemon/axidb-go-protocol/v1/body"
+	"github.com/dejitarudemon/axidb-go-protocol/v1/buffer"
+	"github.com/dejitarudemon/axidb-go-protocol/v1/command"
+	"github.com/dejitarudemon/axidb-go-protocol/v1/err/errs"
+	"github.com/dejitarudemon/axidb-go-protocol/v1/types"
+	"github.com/dejitarudemon/axidb-go-protocol/v1/value"
+)
+
+const (
+	KeyLenFieldSize = 4
+)
+
+var _ body.Body = Write{}
+
+type Write struct {
+	Key   []byte
+	Value value.V
+}
+
+func (w Write) Size() int {
+	if w.Value == nil {
+		return len(w.Key) + KeyLenFieldSize
+	}
+
+	return len(w.Key) + KeyLenFieldSize + w.Value.Size() + types.FieldSize
+}
+
+func (w Write) Encode(buf buffer.Appender) {
+	buf.AppendUint32(uint32(len(w.Key)))
+	buf.Append(w.Key)
+
+	if w.Value != nil {
+		w.Value.Type().Encode(buf)
+		w.Value.Encode(buf)
+	}
+}
+
+func (w Write) Command() command.Code {
+	return command.Write
+}
+
+func (w Write) IsValid() error {
+	if len(w.Key) == 0 {
+		return errs.NewErrorMalformedValue("expected key, but got nothing")
+	}
+
+	if w.Value == nil {
+		return errs.NewErrorMalformedValue("expected value, but got nil")
+	}
+
+	return w.Value.IsValid()
+}
