4dabb94eacb76e68b23dab0c40f91fdbd0ae932f
Vlad Pep <fine.usr1@yandex.ru>
2026-09-21 01:13:59 +0300

    code: added bodies.WriteAnswer

---
+++ v1/body/bodies/write_answer_test.go
@@ -0,0 +1,97 @@
+package bodies
+
+import (
+	"bytes"
+	"fmt"
+	"testing"
+
+	"github.com/dejitarudemon/axidb-go-protocol/v1/buffer"
+	"github.com/dejitarudemon/axidb-go-protocol/v1/command"
+)
+
+func TestWriteAnswer_Size(t *testing.T) {
+	tests := []struct {
+		w    WriteAnswer
+		want int
+	}{
+		{WriteAnswer{}, 1},
+	}
+
+	for _, tt := range tests {
+		t.Run(
+			fmt.Sprintf("TestWriteAnswer_Size %v", tt.w),
+			func(t *testing.T) {
+				if got := tt.w.Size(); got != tt.want {
+					t.Fatalf("got %v, want %v", got, tt.want)
+				}
+			},
+		)
+	}
+}
+
+func TestWriteAnswer_Encode(t *testing.T) {
+	tests := []struct {
+		w    WriteAnswer
+		want []byte
+	}{
+		{WriteAnswer{}, []byte{0x01}},
+	}
+
+	for _, tt := range tests {
+		t.Run(
+			fmt.Sprintf("TestWriteAnswer_Encode %v", tt.w),
+			func(t *testing.T) {
+				buf := buffer.Mock{}
+				buf.Preallocate(tt.w.Size())
+
+				tt.w.Encode(&buf)
+
+				got := buf.Bytes()
+
+				if !bytes.Equal(got, tt.want) {
+					t.Fatalf("got %q, want %q", got, tt.want)
+				}
+			},
+		)
+	}
+}
+
+func TestWriteAnswer_Command(t *testing.T) {
+	tests := []struct {
+		w    WriteAnswer
+		want command.Code
+	}{
+		{WriteAnswer{}, command.Answer},
+	}
+
+	for _, tt := range tests {
+		t.Run(
+			fmt.Sprintf("TestWriteAnswer_Command %v", tt.w),
+			func(t *testing.T) {
+				if got := tt.w.Command(); got != tt.want {
+					t.Fatalf("got %v, want %v", got, tt.want)
+				}
+			},
+		)
+	}
+}
+
+func TestWriteAnswer_IsValid(t *testing.T) {
+	tests := []struct {
+		w    WriteAnswer
+		want bool
+	}{
+		{WriteAnswer{}, false},
+	}
+
+	for _, tt := range tests {
+		t.Run(
+			fmt.Sprintf("TestWriteAnswer_IsValid %v", tt.w),
+			func(t *testing.T) {
+				if got := tt.w.IsValid(); got == nil == tt.want {
+					t.Fatalf("got %v, want %v", got, tt.want)
+				}
+			},
+		)
+	}
+}
