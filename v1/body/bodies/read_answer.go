80c4b123a58ebb73562452715ca9b74ed201d891
Vlad Pep <fine.usr1@yandex.ru>
2026-09-21 20:28:14 +0300

    code: added bodies.ReadAnswer

---
+++ v1/body/bodies/read_answer.go
@@ -0,0 +1,49 @@
+package bodies
+
+import (
+	"github.com/dejitarudemon/axidb-go-protocol/v1/body"
+	"github.com/dejitarudemon/axidb-go-protocol/v1/buffer"
+	"github.com/dejitarudemon/axidb-go-protocol/v1/command"
+	"github.com/dejitarudemon/axidb-go-protocol/v1/err"
+	"github.com/dejitarudemon/axidb-go-protocol/v1/types"
+	"github.com/dejitarudemon/axidb-go-protocol/v1/value"
+)
+
+var _ body.Body = ReadAnswer{}
+
+type ReadAnswer struct {
+	Value value.V
+}
+
+func (r ReadAnswer) Size() int {
+	if r.Value == nil {
+		return ResultFieldSize
+	}
+
+	return ResultFieldSize + types.FieldSize + r.Value.Size()
+}
+
+func (r ReadAnswer) Encode(buf buffer.Appender) {
+	buf.Append(ResultOK)
+
+	if r.Value != nil {
+		r.Value.Type().Encode(buf)
+		r.Value.Encode(buf)
+	}
+
+}
+
+func (r ReadAnswer) Command() command.Code {
+	return command.Answer
+}
+
+func (r ReadAnswer) IsValid() error {
+	if r.Value == nil {
+		return err.NewValidationError(
+			"nil value",
+			"target", r.Command(),
+		)
+	}
+
+	return nil
+}
