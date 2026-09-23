9eda11389a046c84e6d69c485a89f549a2bf250e
Vlad Pep <fine.usr1@yandex.ru>
2026-09-24 01:10:15 +0300

    refactor: add const for RequestBodyLenFieldSize

--- v1/body/bodies/batch.go
+++ v1/body/bodies/batch.go
@@ -8,8 +8,13 @@ import (
 )
 
 const (
-	FlagsFieldSize       = 1
-	RequestsLenFieldSize = 4
+	FlagsFieldSize          = 1
+	RequestsLenFieldSize    = 4
+	RequestBodyLenFieldSize = 4
+
+	IsSequentialExecution = 0b1
+	InterruptAfterError   = 0b10
+	IsOneAnswer           = 0b100
 )
 
 type Request struct {
@@ -19,9 +24,9 @@ type Request struct {
 
 func (r Request) Size() int {
 	if r.Body == nil {
-		return fields.RequestNumberFieldSize + fields.CommandFieldSize + fields.RequestNumberFieldSize
+		return RequestBodyLenFieldSize + fields.CommandFieldSize + fields.RequestNumberFieldSize
 	}
-	return fields.RequestNumberFieldSize + fields.CommandFieldSize + fields.RequestNumberFieldSize + r.Body.Size()
+	return RequestBodyLenFieldSize + fields.CommandFieldSize + fields.RequestNumberFieldSize + r.Body.Size()
 }
 
 func (r Request) Encode(buf buffer.Appender) {
@@ -77,15 +82,15 @@ func (b Batch) encodeFlags() uint8 {
 	encoded := uint8(0)
 
 	if b.IsSequentialExecution {
-		encoded |= 0b1
+		encoded |= IsSequentialExecution
 	}
 
 	if b.InterruptAfterError {
-		encoded |= 0b10
+		encoded |= InterruptAfterError
 	}
 
 	if b.IsOneAnswer {
-		encoded |= 0b100
+		encoded |= IsOneAnswer
 	}
 
 	return encoded
